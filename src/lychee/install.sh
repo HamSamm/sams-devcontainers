f7a221ae899fe6e04309495cc16ac0ed3c4e3424
Samuel Cai <scai043@uottawa.ca>
2026-05-28 10:48:58 -0400

    Create install.sh

---
+++ src/lychee/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+# Get version from options (defined in devcontainer-feature.json)
+VERSION=${VERSION:-"latest"}
+
+# Detect architecture
+architecture=$(uname -m)
+case ${architecture} in
+    x86_64)  arch="x86_64" ;;
+    aarch64|arm64) arch="aarch64" ;;
+    *) echo "Architecture ${architecture} not supported"; exit 1 ;;
+esac
+
+# Download URL
+if [ "${VERSION}" = "latest" ]; then
+    URL="https://github.com/lycheeverse/lychee/releases/latest/download/lychee-${arch}-unknown-linux-gnu.tar.gz"
+else
+    URL="https://github.com/lycheeverse/lychee/releases/download/${VERSION}/lychee-${arch}-unknown-linux-gnu.tar.gz"
+fi
+
+echo "Downloading Lychee from ${URL}..."
+curl -sSL "${URL}" | tar -xz
+mv lychee /usr/local/bin/
+chmod +x /usr/local/bin/lychee
+
+echo "Lychee $(lychee --version) installed!"
