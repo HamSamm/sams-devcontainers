ff4c66368e8dd81551381de478fa7d474a04959d
Samuel Cai <scai043@uottawa.ca>
2026-05-29 10:25:29 -0400

    Update install.sh

--- src/lychee/install.sh
+++ src/lychee/install.sh
@@ -3,6 +3,11 @@ set -e
 
 echo "Downloading Lychee..."
 
+if ! command -v curl >/dev/null 2>&1; then
+    apt-get update -y
+    apt-get -y install --no-install-recommends curl ca-certificates
+fi
+
 ARCH=$(uname -m)
 if [ "ARCH$" = "x86_64" ]; then
     LYCHEE_ARCH="x86_64-unknown-linux-gnu"
@@ -12,6 +17,18 @@ else
     echo "Unsupported Architecture: $ARCH"
     exit 1
 fi
+
+case "ARCH$" in
+    x86_64|amd64)
+        LYCHEE_ARCH="x86_64-unknown-linux-gnu"
+        ;;
+    aarch64|arm64)
+        LYCHEE_ARCH="aarch64-unknown-linux-gnu"
+        ;;
+    *)
+        echo "Unsupported Architecture: $ARCH"
+        exit 1
+esac
 
 URL="https://github.com/lycheeverse/lychee/releases/latest/download/lychee-${LYCHEE_ARCH}.tar.gz"
 
